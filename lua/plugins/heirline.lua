f5f83ff5f2d4bf4dc410314349af2d5eed7055f0
hyperstown <experynt@gmail.com>
2026-02-04 21:44:36 +0100

    add heirline git remote status

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -8,24 +8,22 @@ return {
       opts = opts or {}
       opts.env_names = {} -- FORCE EMPTY
 
-      return function ()
+      return function()
         local conda = vim.env.CONDA_DEFAULT_ENV
         local venv = vim.env.VIRTUAL_ENV
         local version_str = ""
         if venv or (opts.conda and opts.conda.enabled and conda) then
           python = require("venv-selector").python()
-          local result = vim.system({ python, "--version" }, { text = true }):wait()
-          if result.code == 0 and result.stdout ~= "" then
-            version_str = vim.trim(result.stdout)
-          end
+          local result = vim.system(
+            { python, "-c", "import sys; print('.'.join(map(str, sys.version_info[:3])))" }, { text = true }
+          ):wait()
+          if result.code == 0 and result.stdout ~= "" then version_str = vim.trim(result.stdout) end
         end
-        local venv_type = (venv and "venv") or (opts.conda and opts.conda.enabled and conda and "conda") 
+        local venv_type = (venv and "venv") or (opts.conda and opts.conda.enabled and conda and "conda")
         local name = original_virtual_env(opts)()
-        if not name then
-          return name
-        end
+        if not name then return name end
         if venv_type or version_str ~= "" then
-          venv_type = " - " .. version_str .. (venv_type and "(" .. (venv_type or "") .. ")") 
+          venv_type = " " .. (venv_type and "(" .. (venv_type or "") .. ") | ") .. version_str
         end
         return name .. (venv_type or "")
       end
@@ -38,46 +36,42 @@ return {
       local sw = vim.bo.shiftwidth
       local et = vim.bo.expandtab and "Spaces" or "Tabs"
 
-      local text = string.format("%s [%s] ", enc:upper(), fmt:upper())
-      if sw > 0 then
-        text = text .. string.format("%s: %d", et, sw)
-      end
+      local icons = { unix = "󰌽", dos = "", mac = "" }
+      local fmt_display = icons[fmt] or fmt:upper()
+      local segments = {}
+
+      if enc and enc ~= "" then table.insert(segments, enc) end
+      if fmt_display and fmt_display ~= "" then table.insert(segments, fmt_display) end
+      if sw and sw > 0 then table.insert(segments, string.format("%s:%d", et, sw)) end
+      local text = table.concat(segments, " | ")
       return require("astroui.status.utils").stylize(text, opts)
     end
 
     local git_ahead_behind = {
-      condition = function()
-        return require("astroui.status.condition").is_git_repo()
-      end,
+      condition = function() return vim.fn.executable("git") == 1 end,
       init = function(self)
-        self.status = vim.g.git_changes or {"0", "0"}
+        self.status = vim.g.git_changes or { "0", "0" }
         self.ahead = self.status[1]
         self.behind = self.status[2]
       end,
-      provider = function(self)
-        return string.format(" %d %d", self.behind, self.ahead)
-      end,
-      padding = { left = 1, right = 1 },
-      hl = { fg = "fg" },
+      provider = function(self) return string.format(" %d %d ", self.behind, self.ahead) end,
       on_click = {
         callback = function()
+          vim.notify("Started!", vim.log.levels.INFO, { title = "Git Sync" })
           vim.system({ "git", "pull", "--rebase" }, {}, function(res)
             if res.code ~= 0 then
-              local msg = vim.trim(res.stderr ~= "" and res.stderr or res.stdout)
-              vim.schedule(function()
-                vim.notify(
-                  "Git pull failed:\n" .. msg,
-                  vim.log.levels.ERROR,
-                  { title = "Git Sync" }
-                )
-              end)
+              local msg = vim.trim(res.stderr ~= "" and res.stderr or res.stdout or "")
+              vim.schedule(
+                function() vim.notify("Git pull failed:\n" .. msg, vim.log.levels.ERROR, { title = "Git Sync" }) end
+              )
               return
             end
             vim.system({ "git", "push" })
+            vim.notify("Success!", vim.log.levels.INFO, { title = "Git Sync" })
           end)
         end,
         name = "git_sync",
-      }
+      },
     }
 
     opts.statusline = {
@@ -86,7 +80,7 @@ return {
         padding = { right = 1 },
         -- enable mode text with padding as well as an icon before it
         mode_text = {
-          icon = { kind = "VimIcon", padding = { right = 1, left = 1 }, },
+          icon = { kind = "VimIcon", padding = { right = 1, left = 1 } },
         },
         -- surround the component with a separators
         surround = {
@@ -96,20 +90,20 @@ return {
       }),
 
       status.component.file_info({
-          -- enable the file_icon and disable the highlighting based on filetype
-          filename = { fallback = "Empty" },
-          -- disable some of the info
-          filetype = false,
-          file_read_only = false,
-          -- add padding
-          padding = { right = 1 },
-          -- define the section separator
-          surround = { separator = "left", condition = false },
-        }),
+        -- enable the file_icon and disable the highlighting based on filetype
+        filename = { fallback = "Empty" },
+        -- disable some of the info
+        filetype = false,
+        file_read_only = false,
+        -- add padding
+        padding = { right = 1 },
+        -- define the section separator
+        surround = { separator = "left", condition = false },
+      }),
 
       status.component.git_branch(),
-      status.component.git_diff(),
       git_ahead_behind,
+      status.component.git_diff(),
       status.component.diagnostics(),
       status.component.fill(),
       status.component.cmd_info(),
@@ -118,8 +112,9 @@ return {
       status.component.lsp(),
       status.component.virtual_env(),
       status.component.builder({
-        { provider = require('live-server.statusline.astronvim').live_server_provider },
-        padding = { right = 1, left = 1 }
+        { provider = require("live-server.statusline.astronvim").live_server_provider },
+        padding = { right = 1, left = 1 },
+        surround = { separator = "right" },
       }),
       status.component.builder({
         { provider = file_info_provider },
--- lua/polish.lua
+++ lua/polish.lua
@@ -2,10 +2,6 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- TODO move to files
-vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
-vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-
 
 -- TODO create autocmd that executes when git watcher is triggered
 -- api.nvim_exec_autocmds('User', {
@@ -22,24 +18,38 @@ local function refresh_git_changes()
     on_stdout = function(_, data)
       if data and data[1] ~= "" then
         vim.g.git_changes = vim.split(vim.trim(data[1]), "\t")
+        vim.cmd("redrawstatus")
       end
     end,
   })
 end
 
-local timer
 local function debounce(fn, ms)
-  return function()
-    if timer then timer:stop() end
-    timer = vim.uv.new_timer()
-    timer:start(ms, 0, vim.schedule_wrap(fn))
-  end
+  return require('gitsigns.debounce').debounce_trailing(ms, vim.schedule_wrap(fn))
 end
 
 local git_dir = vim.fn.systemlist("git rev-parse --git-dir")[1]
 if git_dir and git_dir ~= "" then
   local handle = vim.uv.new_fs_event()
-  handle:start(git_dir, {}, debounce(refresh_neotree_git, 100))
+  local tree_refresh = debounce(refresh_neotree_git, 100)
+  local status_refresh = debounce(refresh_git_changes, 500)
+  handle:start(git_dir, {}, function() 
+    tree_refresh()
+    status_refresh()
+  end)
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {callback = refresh_git_changes})
+
+
+
+-- Additional useful keymaps
+vim.keymap.set("v", "(", [[c(<C-R>")<ESC>]])
+vim.keymap.set("v", "{", [[c{<C-R>"}<ESC>]])
+vim.keymap.set("v", "[", [[c[<C-R>"]<ESC>]])
+
+vim.keymap.set("v", "'", [[c'<C-R>"'<ESC>]])
+vim.keymap.set("v", '"', [[c"<C-R>""<ESC>]])
+vim.keymap.set("v", '`', [[c`<C-R>"`<ESC>]])
+
+vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
